6c8bfe33daa2e7e87a1fe7611ad2dd50ad7f50af
Albert Örwall <albert@platform24.com>
2023-08-21 09:18:38 +0200

    Rename benchmarks, add GPT3.5 run

--- benchmark-results/2023-08-20-19-12-33--ghostcoder--gpt-3.5-turbo-16k/pangram/pangram.py
+++
@@ -1,7 +0,0 @@
-def is_pangram(sentence):
-    alphabet = "abcdefghijklmnopqrstuvwxyz"
-    sentence = sentence.lower()
-    for char in alphabet:
-        if char not in sentence:
-            return False
-    return True
